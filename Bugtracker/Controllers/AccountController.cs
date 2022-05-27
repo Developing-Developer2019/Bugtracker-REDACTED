7a670e893a2f14e358fd6dcafe8933582feee60d
Marvin Brereton <marvin.brereton@gmail.com>
2022-05-27 10:40:44 +0100

    Moved Error, AccessDenied and StatusMessage to common area

--- Bugtracker/Controllers/AccountController.cs
+++ Bugtracker/Controllers/AccountController.cs
@@ -102,18 +102,19 @@ namespace Bugtracker.Controllers
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = userId, code = code },
-                        protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(account.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    //var callbackUrl = Url.Page(
+                    //    "/Account/ConfirmEmail",
+                    //    pageHandler: null,
+                    //    values: new { area = "Identity", userId = userId, code = code },
+                    //    protocol: Request.Scheme);
+
+                    //await _emailSender.SendEmailAsync(account.Email, "Confirm your email",
+                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
-                        return RedirectToPage("RegisterConfirmation", new { email = account.Email });
+                        return View("RegisterConfirmation.cshtml");
                     }
                     else
                     {
